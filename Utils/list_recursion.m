7cfaa34ddd3b92a06304d24765ba83d476f804c1
Josh Laughner <joshlaugh5@gmail.com>
2017-10-24 16:11:32 -0700

    Old code in list_recursion removed

--- Utils/list_recursion.m
+++ Utils/list_recursion.m
@@ -132,29 +132,4 @@ else
     end
 end
 
-return 
-
-l = py.list;
-
-if isvector(A)
-    if ~isrow(A); 
-        % Python requires that A be 1-by-N, i.e. a row vector
-        A = A';
-    end
-    l=(py.list(A));
-else
-    sz = size(A);
-    A2 = reshape(A, prod(sz(1:end-1)), sz(end));
-    for a=1:size(A, ndims(A))
-        % ismatrix is true if ndims(A) <= 2
-        % Test A not A2 because A2 will always be a matrix
-        if ~ismatrix(A) 
-            Aslice = reshape(A2(:,a), sz(1:end-1));
-        else
-            Aslice = A2(:,a);
-        end
-        l.append(list_recursion(Aslice));
-    end
-end
-
 end
